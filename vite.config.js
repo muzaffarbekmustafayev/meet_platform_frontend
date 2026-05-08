8e0ef4f0656c7476edbe4b1efab12b66fe20d09c
Muzaffar Mustafayev <muzaffarbekmustafayev@gmail.com>
2026-05-08 21:46:20 +0500

    vite.config.json updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -15,4 +14,9 @@ export default defineConfig({
       },
     }),
   ],
+  // Buni qo'shing:
+  base: '/', 
+  build: {
+    outDir: 'dist', // Build natijasi aynan dist papkasiga tushishini kafolatlaydi
+  }
 })
